c2e5ec02ddf90117dedcafedab989efa76f57420
akramansaritorinit <akram.ansari@torinit.ca>
2022-09-08 13:04:03 +0530

    changed emp dashboard,sidebar and colors

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,25 +1,31 @@
 import { Box, Button, Grid } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
-import Typography from "@mui/material/Typography";
 export default function SideBar() {
   return (
-    <Grid item xs={1.5} sx={{ height: "88vh", bgcolor: "#0369A1" }}>
+    <Grid item xs={1.5} sx={{ height: "88vh", bgcolor: "#2f2fa2" }}>
       <Box display="flex" flexDirection="column" justifyContent="center">
-        <Button component={Link} to="/employee/profile">
-          <Typography p={3} variant="h5" color="white">
-            Profile
-          </Typography>
+        <Button
+          sx={{ marginY: 2, fontSize: "20px", color: "white" }}
+          variant="text"
+          component={Link}
+          to="/employee/profile"
+        >
+          Profile
         </Button>
-        <Button component={Link} to="/employee/asset">
-          <Typography p={3} variant="h5" color="white">
-            Asset
-          </Typography>
+        <Button
+          sx={{ marginY: 2, fontSize: "20px", color: "white" }}
+          component={Link}
+          to="/employee/asset"
+        >
+          Asset
         </Button>
-        <Button component={Link} to="/employee/ticket">
-          <Typography p={3} variant="h5" color="white">
-            Request
-          </Typography>
+        <Button
+          sx={{ marginY: 2, fontSize: "20px", color: "white" }}
+          component={Link}
+          to="/employee/ticket"
+        >
+          Request
         </Button>
       </Box>
     </Grid>
