b38850e3f74541b42fd1986411aa6f74a113d8cc
Pratiksha Sontakke <pratiksha.sontakke@torinit.ca>
2022-09-08 17:15:29 +0530

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import AdminDashboard from "./pages/admin/Dashboard";
 import Dashboard from "./pages/employee/Dashboard";
 import Profile from "./pages/employee/Profile";
 import Asset from "./pages/employee/Asset";
 import Ticket from "./pages/employee/Ticket";
+import Home from "./pages/admin/Dashboard";
+import EmpList from "./pages/admin/EmpList";
+import Assets from "./pages/admin/Assets";
+import Services from "./pages/admin/Services";
 function App() {
   return (
     <>
@@ -16,6 +21,12 @@ function App() {
           <Route path="/employee/profile" element={<Profile />} />
           <Route path="/employee/asset" element={<Asset />} />
           <Route path="/employee/ticket" element={<Ticket />} />
+          <Route path="/admin/home" element={<Home />} />
+
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/employee" element={<EmpList />} />
+          <Route path="/admin/assets" element={<Assets />} />
+          <Route path="/admin/service" element={<Services />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,6 +3,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 export default function SideBar() {
+  const isAdmin = true;
+  return isAdmin ? <AdminSidebar /> : <EmployeeSidebar />;
+}
+const AdminSidebar = () => {
+  return (
+    <Grid item xs={1.5} sx={{ height: "88vh", bgcolor: "#0369A1" }}>
+      <Box display="flex" flexDirection="column" justifyContent="center">
+        <Button component={Link} to="/employee/profile">
+          <Typography p={3} variant="h5" color="white">
+            Dashboard
+          </Typography>
+        </Button>
+        <Button component={Link} to="/employee/asset">
+          <Typography p={3} variant="h5" color="white">
+            Employee
+          </Typography>
+        </Button>
+        <Button component={Link} to="/employee/ticket">
+          <Typography p={3} variant="h5" color="white">
+            Asset
+          </Typography>
+        </Button>
+        <Button component={Link} to="/employee/ticket">
+          <Typography p={3} variant="h5" color="white">
+            Service
+          </Typography>
+        </Button>
+      </Box>
+    </Grid>
+  );
+};
+const EmployeeSidebar = () => {
   return (
     <Grid item xs={1.5} sx={{ height: "88vh", bgcolor: "#0369A1" }}>
       <Box display="flex" flexDirection="column" justifyContent="center">
@@ -24,4 +56,4 @@ export default function SideBar() {
       </Box>
     </Grid>
   );
-}
+};
