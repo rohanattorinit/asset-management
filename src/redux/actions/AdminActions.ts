f3cd7514bb8c8f372db5d9220621f6b720ffb84e
Rohan Desai <111341927+rohanattorinit@users.noreply.github.com>
2022-09-14 18:49:21 +0530

    added create employee functionality

--- src/redux/actions/AdminActions.ts
+++ src/redux/actions/AdminActions.ts
@@ -1,6 +1,8 @@
+import { SET_ADDASSET, SET_ADDEMPLOYEE } from "./../types";
 import axios from "axios";
 import { Dispatch } from "redux";
 import {
+  CreateEmployeeType,
   DispatchTypes,
   LOADING_DATA,
   SET_ASSETS,
@@ -33,3 +35,39 @@ export const getAssets = () => async (dispatch: Dispatch<DispatchTypes>) => {
     });
   }
 };
+
+export const addEmployee =
+  (employeeDetails: CreateEmployeeType) =>
+  async (dispatch: Dispatch<DispatchTypes>) => {
+    dispatch({ type: LOADING_DATA });
+    try {
+      const res = await axios.post(
+        "http://localhost:4000/api/employees",
+        employeeDetails
+      );
+      dispatch({ type: SET_ADDEMPLOYEE, payload: res.data });
+    } catch (error) {
+      dispatch({
+        type: SET_ERROR,
+        payload: (error as any).response.data.error,
+      });
+    }
+  };
+
+export const AddAsset =
+  (assetDetails: CreateEmployeeType) =>
+  async (dispatch: Dispatch<DispatchTypes>) => {
+    dispatch({ type: LOADING_DATA });
+    try {
+      const res = await axios.post(
+        "http://localhost:4000/api/assets/addAsset",
+        assetDetails
+      );
+      dispatch({ type: SET_ADDASSET, payload: res.data });
+    } catch (error) {
+      dispatch({
+        type: SET_ERROR,
+        payload: (error as any).response.data.error,
+      });
+    }
+  };
