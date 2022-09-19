5f7359c3e66b0f56c75e42cbf5de2a0f7d5bc3f6
Arch1198 <archana.gangurde@torinit.ca>
2022-09-19 19:39:26 +0530

    changes in Ticket pages

--- src/pages/employee/Ticket.tsx
+++ src/pages/employee/Ticket.tsx
@@ -9,7 +9,7 @@ import Typography from "@mui/material/Typography";
 
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { useDispatch } from "react-redux";
-import { Dispatch, useEffect } from "react";
+import { Dispatch, useEffect, useLayoutEffect, useRef } from "react";
 import { getEmployeeTickets } from "../../redux/actions/EmployeeActions";
 import { useSelector } from "react-redux";
 import { RootStore } from "../../redux/store";
@@ -17,46 +17,41 @@ import { RootStore } from "../../redux/store";
 export default function Ticket() {
   const dispatch: Dispatch<any> = useDispatch();
 
-  const { tickets, employee } = useSelector(
+  const { tickets, employee, message } = useSelector(
     (state: RootStore) => state.employee
   );
-
   useEffect(() => {
     dispatch(getEmployeeTickets(employee.empId));
-  }, [dispatch, employee.empId]);
-
+  }, [dispatch, employee, message]);
   return (
     <Grid container>
       <Sidebar />
-      <Grid item xs={12} md={10}>
-        <Typography variant="h4" textAlign="center" marginY={5}>
+      <Grid item xs={12} md={10} sx={{ overflowX: "auto" }}>
+        <Typography variant="h4" textAlign="center" marginY={2}>
           Ticket Status
         </Typography>
-        <Grid
-          container
-          direction="row"
-          justifyContent="space-evenly"
-          alignItems="center"
-        >
+        <Grid container spacing={5}>
           {tickets.map((tickets) => {
             return (
-              <Card
-                sx={{ width: "18rem", height: "15rem" }}
-                key={tickets.ticketId}
-              >
-                <CardHeader title={"#" + tickets.ticketId} />
-                <CardContent>
-                  <Typography variant="body1">
-                    Title : {tickets.title}
-                  </Typography>
-                  <Typography variant="body1">
-                    Description : {tickets.description}
-                  </Typography>
-                  <Typography variant="body1">
-                    Status : {tickets.ticketStatus}
-                  </Typography>
-                </CardContent>
-              </Card>
+              <Grid item xs={6} md={4}>
+                <Card key={tickets.ticketId}>
+                  {/* <CardHeader title={"#" + tickets.ticketId} /> */}
+                  <CardContent>
+                    <Typography variant="h5">
+                      {"# " + tickets.ticketId}
+                    </Typography>
+                    <Typography variant="body1">
+                      Title : {tickets.title}
+                    </Typography>
+                    <Typography variant="body1">
+                      Description : {tickets.description.slice(0, 20)}
+                    </Typography>
+                    <Typography variant="body1">
+                      Status : {tickets.ticketStatus}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
             );
           })}
         </Grid>
