d67b6dda4952097ad10612b096f033129924bffb
Pratiksha Sontakke <pratiksha.sontakke@torinit.ca>
2022-09-20 11:25:35 +0530

    merging with main

--- src/pages/employee/Ticket.tsx
+++ src/pages/employee/Ticket.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 
 import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
 
 import CardContent from "@mui/material/CardContent";
 import { Grid } from "@mui/material";
@@ -9,7 +8,7 @@ import Typography from "@mui/material/Typography";
 
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { useDispatch } from "react-redux";
-import { Dispatch, useEffect, useLayoutEffect, useRef } from "react";
+import { Dispatch, useEffect } from "react";
 import { getEmployeeTickets } from "../../redux/actions/EmployeeActions";
 import { useSelector } from "react-redux";
 import { RootStore } from "../../redux/store";
@@ -59,70 +58,3 @@ export default function Ticket() {
     </Grid>
   );
 }
-
-// import { Grid } from "@mui/material";
-// import { useState } from "react";
-// import Sidebar from "../../components/Sidebar/Sidebar";
-// import Button from "@mui/material/Button";
-// import { Box } from "@mui/material";
-// import TextField from "@mui/material/TextField";
-
-// export default function Ticket() {
-//   // const [textValue, setTextValue] = useState<string>("");
-
-//   // const onTextChange = (e: any) => setTextValue(e.target.value);
-
-//   return (
-//     <Grid container>
-//       <Sidebar />
-//       {/* <Grid item xs={12} md={10} p={3}>
-//         <Grid container justifyContent="center">
-//           <Grid
-//             item
-//             xs={12}
-//             md={6}
-//             sx={{
-//               display: "flex",
-//               flexDirection: "column",
-//             }}
-//           >
-//             <Box
-//               sx={{
-//                 display: "flex",
-//                 justifyContent: "space-between",
-//                 marginY: 3,
-//               }}
-//             >
-//               <Button variant="outlined">Ticket Request</Button>
-//               <Button variant="outlined">Ticket Status</Button>
-//             </Box>
-//             <TextField
-//               sx={{ marginTop: "5" }}
-//               onChange={onTextChange}
-//               value={textValue}
-//               label={"Asset ID"} //optional
-//             />
-//             <TextField
-//               sx={{ mt: 3 }}
-//               onChange={onTextChange}
-//               value={textValue}
-//               label={"Request Subject"} //optional
-//             />
-//             <TextField
-//               sx={{ mt: 3 }}
-//               onChange={onTextChange}
-//               value={textValue}
-//               label={"Describe Issue"}
-//               multiline
-//               rows={4} //optional
-//             />
-//             <Box sx={{ mt: 3 }} textAlign="center">
-//               <Button variant="contained">Create Request</Button>
-//             </Box>
-//           </Grid>
-//         </Grid>
-//       </Grid>*/}
-
-//     </Grid>
-//   );
-// }
