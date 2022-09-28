c32ea015f070e39b40deccef9cec58255d7bcbbd
akramansaritorinit <akram.ansari@torinit.ca>
2022-09-28 15:40:22 +0530

    colors added according to ticket status

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 import { useTheme, useMediaQuery } from "@mui/material";
 
 import {
-  Drawer,
-  IconButton,
-  List,
-  ListItemButton,
-  ListItemText,
-  Toolbar,
+   Drawer,
+   IconButton,
+   List,
+   ListItemButton,
+   ListItemText,
+   Toolbar,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useSelector } from "react-redux";
@@ -17,156 +17,164 @@ import { RootStore } from "../../redux/store";
 import { SideNavGrid } from "../Styled/StyledComponent";
 
 export default function SideBar() {
-  const {
-    user: { isAdmin },
-  } = useSelector((state: RootStore) => state.login);
-  const [openDrawer, setOpenDrawer] = useState(false);
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.down("md"));
+   const {
+      user: { isAdmin },
+   } = useSelector((state: RootStore) => state.login);
+   const [openDrawer, setOpenDrawer] = useState(false);
+   const theme = useTheme();
+   const matches = useMediaQuery(theme.breakpoints.down("md"));
 
-  const MobileNav = () => {
-    return (
-      <Toolbar>
-        <Drawer
-          open={openDrawer}
-          onClose={() => setOpenDrawer(false)}
-          anchor="right"
-        >
-          {isAdmin ? (
-            <List>
-              <ListItemButton component={Link} to="/">
-                <ListItemText>Dashboard</ListItemText>
-              </ListItemButton>
+   const MobileNav = () => {
+      return (
+         <Toolbar>
+            <Drawer
+               open={openDrawer}
+               onClose={() => setOpenDrawer(false)}
+               anchor="right"
+            >
+               {isAdmin ? (
+                  <List>
+                     <ListItemButton component={Link} to="/">
+                        <ListItemText>Dashboard</ListItemText>
+                     </ListItemButton>
 
-              <ListItemButton component={Link} to="/admin/employee">
-                <ListItemText>Employee</ListItemText>
-              </ListItemButton>
+                     <ListItemButton component={Link} to="/admin/employee">
+                        <ListItemText>Employee</ListItemText>
+                     </ListItemButton>
 
-              <ListItemButton component={Link} to="/admin/assets">
-                <ListItemText>Assets</ListItemText>
-              </ListItemButton>
-              <ListItemButton component={Link} to="/admin/service">
-                <ListItemText>Services</ListItemText>
-              </ListItemButton>
-              <ListItemButton component={Link} to="/">
-                <ListItemText>logout</ListItemText>
-              </ListItemButton>
-            </List>
-          ) : (
-            <List>
-              <ListItemButton component={Link} to="/profile">
-                <ListItemText>Profile</ListItemText>
-              </ListItemButton>
+                     <ListItemButton component={Link} to="/admin/assets">
+                        <ListItemText>Assets</ListItemText>
+                     </ListItemButton>
+                     <ListItemButton component={Link} to="/admin/service">
+                        <ListItemText>Services</ListItemText>
+                     </ListItemButton>
+                     <ListItemButton component={Link} to="/">
+                        <ListItemText>logout</ListItemText>
+                     </ListItemButton>
+                  </List>
+               ) : (
+                  <List>
+                     <ListItemButton component={Link} to="/profile">
+                        <ListItemText>Profile</ListItemText>
+                     </ListItemButton>
 
-              <ListItemButton component={Link} to="/asset">
-                <ListItemText>Assets</ListItemText>
-              </ListItemButton>
+                     <ListItemButton component={Link} to="/asset">
+                        <ListItemText>Assets</ListItemText>
+                     </ListItemButton>
 
-              <ListItemButton component={Link} to="/ticket">
-                <ListItemText>Ticket</ListItemText>
-              </ListItemButton>
-              <Divider />
-              <ListItemButton component={Link} to="/">
-                <ListItemText>Logout</ListItemText>
-              </ListItemButton>
-            </List>
-          )}
-        </Drawer>
-        <IconButton
-          size="large"
-          sx={{ position: "fixed", right: 10, top: 30, color: "white" }}
-          onClick={() => setOpenDrawer(true)}
-        >
-          <MenuIcon />
-        </IconButton>
-      </Toolbar>
-    );
-  };
+                     <ListItemButton component={Link} to="/ticket">
+                        <ListItemText>Ticket</ListItemText>
+                     </ListItemButton>
+                     <Divider />
+                     <ListItemButton component={Link} to="/">
+                        <ListItemText>Logout</ListItemText>
+                     </ListItemButton>
+                  </List>
+               )}
+            </Drawer>
+            <IconButton
+               size="large"
+               sx={{ position: "fixed", right: 10, top: 30, color: "white" }}
+               onClick={() => setOpenDrawer(true)}
+            >
+               <MenuIcon />
+            </IconButton>
+         </Toolbar>
+      );
+   };
 
-  const SideNav = () => {
-    return (
-      <SideNavGrid
-        item
-        xs={12}
-        md={2}
-        sx={{
-          bgcolor: "#011E41",
-          borderTop: "solid white 2px",
-        }}
-      >
-        {isAdmin ? (
-          <Box display="flex" flexDirection="column" justifyContent="center">
-            <Button
-              sx={{ marginY: 2, marginX: 2, fontSize: "20px" }}
-              variant="outlined"
-              color="secondary"
-              component={Link}
-              to="/"
-            >
-              Dashboard
-            </Button>
-            <Button
-              sx={{ marginY: 2, marginX: 2, fontSize: "20px" }}
-              variant="outlined"
-              color="secondary"
-              component={Link}
-              to="/admin/employee"
-            >
-              Employee
-            </Button>
-            <Button
-              sx={{ marginY: 2, marginX: 2, fontSize: "20px" }}
-              variant="outlined"
-              color="secondary"
-              component={Link}
-              to="/admin/assets"
-            >
-              Assets
-            </Button>
-            <Button
-              sx={{ marginY: 2, marginX: 2, fontSize: "20px" }}
-              variant="outlined"
-              color="secondary"
-              component={Link}
-              to="/admin/service"
-            >
-              Services
-            </Button>
-          </Box>
-        ) : (
-          <Box display="flex" flexDirection="column" justifyContent="center">
-            <Button
-              sx={{ marginY: 2, marginX: 2, fontSize: "20px" }}
-              variant="outlined"
-              color="secondary"
-              component={Link}
-              to="/profile"
-            >
-              Profile
-            </Button>
-            <Button
-              sx={{ marginY: 2, marginX: 2, fontSize: "20px" }}
-              variant="outlined"
-              color="secondary"
-              component={Link}
-              to="/asset"
-            >
-              Asset
-            </Button>
-            <Button
-              sx={{ marginY: 2, marginX: 2, fontSize: "20px" }}
-              variant="outlined"
-              color="secondary"
-              component={Link}
-              to="/ticket"
-            >
-              Requests
-            </Button>
-          </Box>
-        )}
-      </SideNavGrid>
-    );
-  };
+   const SideNav = () => {
+      return (
+         <SideNavGrid
+            item
+            xs={12}
+            md={2}
+            sx={{
+               bgcolor: "#011E41",
+               borderTop: "solid white 2px",
+            }}
+         >
+            {isAdmin ? (
+               <Box
+                  display="flex"
+                  flexDirection="column"
+                  justifyContent="center"
+               >
+                  <Button
+                     sx={{ marginY: 2, marginX: 2, fontSize: "20px" }}
+                     variant="outlined"
+                     color="secondary"
+                     component={Link}
+                     to="/"
+                  >
+                     Dashboard
+                  </Button>
+                  <Button
+                     sx={{ marginY: 2, marginX: 2, fontSize: "20px" }}
+                     variant="outlined"
+                     color="secondary"
+                     component={Link}
+                     to="/admin/employee"
+                  >
+                     Employee
+                  </Button>
+                  <Button
+                     sx={{ marginY: 2, marginX: 2, fontSize: "20px" }}
+                     variant="outlined"
+                     color="secondary"
+                     component={Link}
+                     to="/admin/assets"
+                  >
+                     Assets
+                  </Button>
+                  <Button
+                     sx={{ marginY: 2, marginX: 2, fontSize: "20px" }}
+                     variant="outlined"
+                     color="secondary"
+                     component={Link}
+                     to="/admin/service"
+                  >
+                     Services
+                  </Button>
+               </Box>
+            ) : (
+               <Box
+                  display="flex"
+                  flexDirection="column"
+                  justifyContent="center"
+               >
+                  <Button
+                     sx={{ marginY: 2, marginX: 2, fontSize: "20px" }}
+                     variant="outlined"
+                     color="secondary"
+                     component={Link}
+                     to="/profile"
+                  >
+                     Profile
+                  </Button>
+                  <Button
+                     sx={{ marginY: 2, marginX: 2, fontSize: "20px" }}
+                     variant="outlined"
+                     color="secondary"
+                     component={Link}
+                     to="/asset"
+                  >
+                     Asset
+                  </Button>
+                  <Button
+                     sx={{ marginY: 2, marginX: 2, fontSize: "20px" }}
+                     variant="outlined"
+                     color="secondary"
+                     component={Link}
+                     to="/ticket"
+                  >
+                     Requests
+                  </Button>
+               </Box>
+            )}
+         </SideNavGrid>
+      );
+   };
 
-  return matches ? <MobileNav /> : <SideNav />;
+   return matches ? <MobileNav /> : <SideNav />;
 }
--- src/pages/employee/Ticket.tsx
+++ src/pages/employee/Ticket.tsx
@@ -13,74 +13,90 @@ import { RootStore } from "../../redux/store";
 import { EmpTicketType } from "../../redux/types";
 
 export default function Ticket() {
-  const [open, setOpen] = useState(false);
-  const [ticket, setTicket] = useState<EmpTicketType>();
+   const [open, setOpen] = useState(false);
+   const [ticket, setTicket] = useState<EmpTicketType>();
 
-  const dispatch: Dispatch<any> = useDispatch();
+   const dispatch: Dispatch<any> = useDispatch();
 
-  const { tickets, message } = useSelector(
-    (state: RootStore) => state.employee
-  );
-  const { user } = useSelector((state: RootStore) => state.login);
+   const { tickets, message } = useSelector(
+      (state: RootStore) => state.employee
+   );
+   const { user } = useSelector((state: RootStore) => state.login);
 
-  useEffect(() => {
-    dispatch(getEmployeeTickets(user?.empId));
-  }, [dispatch, user, message]);
+   useEffect(() => {
+      dispatch(getEmployeeTickets(user?.empId));
+   }, [dispatch, user, message]);
 
-  const handleClick = (ticketId: number) => {
-    setTicket(tickets?.filter((ticket) => ticket?.ticketId === ticketId)[0]);
-    setOpen(true);
-  };
+   const handleClick = (ticketId: number) => {
+      setTicket(tickets?.filter((ticket) => ticket?.ticketId === ticketId)[0]);
+      setOpen(true);
+   };
 
-  return (
-    <Grid container sx={{ height: "100%" }}>
-      <Sidebar />
-      <Grid item xs={12} md={10} p={3}>
-        <Typography variant="h4" textAlign="center" marginY={2}>
-          Ticket Status
-        </Typography>
-        <Grid container spacing={5}>
-          {tickets?.map((ticket) => {
-            return (
-              <Grid item xs={6} md={4}>
-                <Button onClick={() => handleClick(ticket?.ticketId)}>
-                  <Card key={ticket?.ticketId}>
-                    <CardContent>
-                      <Typography variant="h5">
-                        {"# " + ticket?.ticketId}
-                      </Typography>
-                      <Typography variant="body1">
-                        Title : {ticket?.title}
-                      </Typography>
-                      <Typography variant="body1">
-                        Description : {ticket?.description.slice(0, 10)}...
-                      </Typography>
-                      <Typography variant="body1">
-                        Status : {ticket?.ticketStatus}
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                </Button>
-              </Grid>
-            );
-          })}
-          <Dialog open={open} onClose={() => setOpen(false)}>
-            <DialogContent>
-              <CardContent>
-                <Typography variant="h5">{"# " + ticket?.ticketId}</Typography>
-                <Typography variant="body1">Title : {ticket?.title}</Typography>
-                <Typography variant="body1">
-                  Description : {ticket?.description.slice(0, 50)}
-                </Typography>
-                <Typography variant="body1">
-                  Status : {ticket?.ticketStatus}
-                </Typography>
-                <Typography variant="body1">Note : {ticket?.note}</Typography>
-              </CardContent>
-            </DialogContent>
-          </Dialog>
-        </Grid>
+   return (
+      <Grid container sx={{ height: "100%" }}>
+         <Sidebar />
+         <Grid item xs={12} md={10} p={3}>
+            <Typography variant="h4" textAlign="center" marginY={2}>
+               Ticket Status
+            </Typography>
+            <Grid container spacing={2}>
+               {tickets?.map((ticket) => {
+                  return (
+                     <Grid item minWidth={300}>
+                        <Card
+                           key={ticket?.ticketId}
+                           onClick={() => handleClick(ticket?.ticketId)}
+                           sx={{
+                              bgcolor:
+                                 ticket?.ticketStatus == "active"
+                                    ? "#EF9A9A"
+                                    : ticket?.ticketStatus == "pending"
+                                    ? "#FFE0B2"
+                                    : "#B2DFDB",
+                           }}
+                        >
+                           <CardContent>
+                              <Typography variant="h6">
+                                 {"ID: " + ticket?.ticketId}
+                              </Typography>
+                              <Typography variant="body1">
+                                 Title : {ticket?.title}
+                              </Typography>
+                              <Typography variant="body1">
+                                 Description :{" "}
+                                 {ticket?.description.slice(0, 10)}...
+                              </Typography>
+                              <Typography variant="body1">
+                                 Status : {ticket?.ticketStatus}
+                              </Typography>
+                           </CardContent>
+                        </Card>
+                     </Grid>
+                  );
+               })}
+               <Dialog open={open} onClose={() => setOpen(false)}>
+                  <DialogContent>
+                     <CardContent>
+                        <Typography variant="h6">
+                           {"ID: " + ticket?.ticketId}
+                        </Typography>
+                        <Typography variant="body1">
+                           Title : {ticket?.title}
+                        </Typography>
+                        <Typography variant="body1">
+                           Description : {ticket?.description.slice(0, 50)}
+                        </Typography>
+                        <Typography variant="body1">
+                           Status : {ticket?.ticketStatus}
+                        </Typography>
+                        <Typography variant="body1">
+                           Note : {ticket?.note}
+                        </Typography>
+                     </CardContent>
+                  </DialogContent>
+               </Dialog>
+            </Grid>
+         </Grid>
       </Grid>
-    </Grid>
-  );
+   );
 }
