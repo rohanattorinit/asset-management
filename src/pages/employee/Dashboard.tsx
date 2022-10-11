cceb7b4e40d4665b9b60bd7847a7a160ce9d2939
Pratiksha Sontakke <pratiksha.sontakke@torinit.ca>
2022-10-11 16:33:17 +0530

    added loader on employee side

--- src/pages/employee/Dashboard.tsx
+++ src/pages/employee/Dashboard.tsx
@@ -7,11 +7,14 @@ import { RootStore } from "../../redux/store";
 import SideBar from "../../components/Sidebar/Sidebar";
 import Carousel from "../../components/Carousel/Carousel";
 import Toast from "../../components/ErrorHandling/Toast";
+import Loader from "../../components/Loader/Loader";
 
 export default function Dashboard() {
   const dispatch: Dispatch<any> = useDispatch();
 
-  const { tickets } = useSelector((state: RootStore) => state?.employee);
+  const { tickets, loading } = useSelector(
+    (state: RootStore) => state?.employee
+  );
 
   const { user } = useSelector((state: RootStore) => state.login);
 
@@ -35,30 +38,39 @@ export default function Dashboard() {
           <Typography variant="h5" marginY={2}>
             Your Active Tickets
           </Typography>
-          <Grid container spacing={5}>
-            {filteredStatus?.map((requeststatus) => {
-              return (
-                <Grid item xs={6} md={3}>
-                  <Card key={requeststatus?.ticketId}>
-                    <CardContent>
-                      <Typography variant="h5">
-                        {"# " + requeststatus?.ticketId}
-                      </Typography>
-                      <Typography variant="body1">
-                        Title : {requeststatus?.title}
-                      </Typography>
-                      <Typography variant="body1">
-                        Description : {requeststatus?.description?.slice(0, 20)}
-                      </Typography>
-                      <Typography variant="body1">
-                        Status : {requeststatus?.ticketStatus}
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              );
-            })}
-          </Grid>
+          {filteredStatus.length ? (
+            <Grid container spacing={5}>
+              {loading && filteredStatus.length ? (
+                <Loader />
+              ) : (
+                filteredStatus?.map((requeststatus) => {
+                  return (
+                    <Grid item xs={6} md={3}>
+                      <Card key={requeststatus?.ticketId}>
+                        <CardContent>
+                          <Typography variant="h5">
+                            {"# " + requeststatus?.ticketId}
+                          </Typography>
+                          <Typography variant="body1">
+                            Title : {requeststatus?.title}
+                          </Typography>
+                          <Typography variant="body1">
+                            Description :{" "}
+                            {requeststatus?.description?.slice(0, 20)}
+                          </Typography>
+                          <Typography variant="body1">
+                            Status : {requeststatus?.ticketStatus}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  );
+                })
+              )}
+            </Grid>
+          ) : (
+            <Typography textAlign={"center"}>No Active Tickets!!!</Typography>
+          )}
         </Box>
       </Grid>
     </Grid>
