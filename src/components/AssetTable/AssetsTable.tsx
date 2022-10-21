7963edaa18b7469a64e91abddfa3b3ba4cb72a66
Mahesh Bhadane <108877616+MaheshBhadane@users.noreply.github.com>
2022-10-21 12:00:56 +0530

    Update AssetsTable.tsx

--- src/components/AssetTable/AssetsTable.tsx
+++ src/components/AssetTable/AssetsTable.tsx
@@ -39,65 +39,75 @@ const AssetsTable = () => {
       {loading ? (
         <Loader />
       ) : (
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">
-                  <Typography sx={{ fontWeight: "bold" }}>AssetID</Typography>
-                </TableCell>
-                <TableCell align="center">
-                  <Typography sx={{ fontWeight: "bold" }}>Model No.</Typography>
-                </TableCell>
-                <TableCell align="center">
-                  <Typography sx={{ fontWeight: "bold" }}>
-                    Asset Name
-                  </Typography>
-                </TableCell>
-                <TableCell align="center">
-                  <Typography sx={{ fontWeight: "bold" }}>Category</Typography>
-                </TableCell>
-                <TableCell align="center">
-                  <Typography sx={{ fontWeight: "bold" }}>
-                    Asset Location
-                  </Typography>
-                </TableCell>
-                <TableCell align="center">
-                  <Typography sx={{ fontWeight: "bold" }}>Status</Typography>
-                </TableCell>
-                <TableCell align="center">
-                  <Typography sx={{ fontWeight: "bold" }}>Usability</Typography>
-                </TableCell>
-              </TableRow>
-            </TableHead>
+        <TableContainer sx={{ marginY: 3 }} component={Paper}>
+          {assets.length ? (
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">
+                    <Typography sx={{ fontWeight: "bold" }}>AssetID</Typography>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Typography sx={{ fontWeight: "bold" }}>
+                      Model No.
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Typography sx={{ fontWeight: "bold" }}>
+                      Asset Name
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Typography sx={{ fontWeight: "bold" }}>
+                      Category
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Typography sx={{ fontWeight: "bold" }}>
+                      Asset Location
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Typography sx={{ fontWeight: "bold" }}>Status</Typography>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Typography sx={{ fontWeight: "bold" }}>
+                      Usability
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
 
-            {assets?.map((filteredAsset) => (
-              <TableRow key={filteredAsset?.assetId}>
-                <TableCell align="center">{filteredAsset?.assetId}</TableCell>
-                <TableCell align="center">{filteredAsset?.modelNo}</TableCell>
-                <TableCell align="center">
-                  {filteredAsset?.name?.toUpperCase()}
-                </TableCell>
-                <TableCell align="center">
-                  {filteredAsset?.category?.toUpperCase()}
-                </TableCell>
-                <TableCell align="center">
-                  {filteredAsset?.asset_location?.toUpperCase()}
-                </TableCell>
-                <TableCell align="center">
-                  {filteredAsset?.status?.toUpperCase()}
-                </TableCell>
-                <TableCell align="center">
-                  {filteredAsset?.usability?.toUpperCase()}
-                </TableCell>
-                <IconButton
-                  onClick={() => setAssetDetails(filteredAsset?.assetId)}
-                >
-                  <OpenInNewIcon sx={{ color: "darkblue" }} />
-                </IconButton>
-              </TableRow>
-            ))}
-          </Table>
+              {assets?.map((filteredAsset) => (
+                <TableRow key={filteredAsset?.assetId}>
+                  <TableCell align="center">{filteredAsset?.assetId}</TableCell>
+                  <TableCell align="center">{filteredAsset?.modelNo}</TableCell>
+                  <TableCell align="center">
+                    {filteredAsset?.name?.toUpperCase()}
+                  </TableCell>
+                  <TableCell align="center">
+                    {filteredAsset?.category?.toUpperCase()}
+                  </TableCell>
+                  <TableCell align="center">
+                    {filteredAsset?.asset_location?.toUpperCase()}
+                  </TableCell>
+                  <TableCell align="center">
+                    {filteredAsset?.status?.toUpperCase()}
+                  </TableCell>
+                  <TableCell align="center">
+                    {filteredAsset?.usability?.toUpperCase()}
+                  </TableCell>
+                  <IconButton
+                    onClick={() => setAssetDetails(filteredAsset?.assetId)}
+                  >
+                    <OpenInNewIcon sx={{ color: "darkblue" }} />
+                  </IconButton>
+                </TableRow>
+              ))}
+            </Table>
+          ) : (
+            <Typography textAlign={"center"}>No Assets found!</Typography>
+          )}
         </TableContainer>
       )}
     </>
