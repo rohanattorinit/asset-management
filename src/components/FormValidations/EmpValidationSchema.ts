438f3710275443f7a5c515bb3d160da9915d49fc
Pratiksha Sontakke <pratiksha.sontakke@torinit.ca>
2022-12-01 12:16:46 +0530

    update in emp validation schema

--- src/components/FormValidations/EmpValidationSchema.ts
+++ src/components/FormValidations/EmpValidationSchema.ts
@@ -1,7 +1,8 @@
 import * as Yup from 'yup'
 const numericRegEx = /(?=.*[0-9])/
 const uppercaseRegEx = /(?=.*[A-Z])/
-const re = /^[A-Z/a-z/ \b]+$/
+//  const re = /^[A-Z/a-z/ \b]+$/
+const re =/^[a-zA-Z ]+$/
 const phoneRegExp = /^((?!(0))[0-9]{10})$/
 
 export const EmpValidationSchema = Yup.object().shape({
@@ -10,13 +11,13 @@ export const EmpValidationSchema = Yup.object().shape({
     .matches(uppercaseRegEx, 'Invalid employee ID ')
     .required('EmployeeID Required'),
   name: Yup.string()
-    .matches(re, 'Name can have letters only!')
+    .matches(re, 'Name can have letters only!').min(3,'Name is too small').max(36,'Name is too long')
     .required('Full name required'),
   email: Yup.string()
     .email('Invalid email')
     .required('Email Required'),
   location: Yup.string()
-    .matches(re, 'Location can have letters only!')
+    .matches(re, 'Location can have letters only!').min(3,'Location is too small').max(28,'Location is too long')
     .required('Location Required!'),
   phone: Yup.string()
     .matches(phoneRegExp, 'Invalid phone number')
