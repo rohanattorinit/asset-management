0e7e318fea19af1cd95f9da68309d0490266a946
Mahesh Bhadane <108877616+MaheshBhadane@users.noreply.github.com>
2022-12-12 15:55:15 +0530

    counts in getTotalAssetCategoryCount

--- src/redux/reducers/AdminReducer.ts
+++ src/redux/reducers/AdminReducer.ts
@@ -1,9 +1,7 @@
 import {
   AssetTransactionHistory,
-  
   AssetCategoryCount,
   ASSET_TRANSACTION_HISTORY,
-  
   BrandOptions,
   DELETE_ASSET,
   DELETE_EMPLOYEE,
@@ -35,72 +33,68 @@ import {
   SET_SERVICE_TICKET_DETAILS,
   SET_SINGLE_ASSET_TICKETS,
   Counts,
-  
-} from './../types'
-
+} from "./../types";
 
 interface InitialState {
   loading: boolean;
-  
+
   employees: EmployeeType[];
   assets: AssetTypes[];
   employeeDetails: EmployeeType;
   singleAssetDetails: SingleAssetDetailsType;
-  singleAssetTickets:  ServiceType[];
+  singleAssetTickets: ServiceType[];
   employeeassetsdetails: AllocatedAssetType[];
   serviceDetails: ServiceType[];
   serviceticketdetails: ServiceType;
   error?: string;
   message: string;
   brandOptions: BrandOptions[];
-  totalAssetCount : AssetCategoryCount[]
-  totalSurplusAssetCount : AssetCategoryCount[]
-  counts: Counts
+  totalAssetCount: AssetCategoryCount[];
+  totalSurplusAssetCount: AssetCategoryCount[];
+  counts: Counts;
   filterOptions: FilterOptions;
-  
-  assetTrasactionLogs:AssetTransactionHistory[];
-  
+  assetTrasactionLogs: AssetTransactionHistory[];
 }
 
 const initialState: InitialState = {
   loading: false,
-  
+
   employees: [],
   assets: [],
-  message: '',
-  error: '',
+  message: "",
+  error: "",
   employeeDetails: {
-    empId: '',
-    name: '',
-    email: '',
-    phone: '',
-    location: '',
+    empId: "",
+    name: "",
+    email: "",
+    phone: "",
+    location: "",
     isAdmin: false,
-    jobTitle: ''
+    jobTitle: "",
   },
   singleAssetDetails: {
     assetId: 0,
-    brandName: '',
-    name: '',
+    brandName: "",
+    name: "",
     modelNo: 0,
-    description: '',
-    status: '',
+    description: "",
+    status: "",
     isRented: 0,
-    empName: '',
-    empId: '',
-    vendor: '',
+    empName: "",
+    empId: "",
+    vendor: "",
     rent: 0,
     deposit: 0,
-    rentStartDate: '',
-    rentEndDate: '',
-    asset_location: '',
-    processor: '',
-    screen_type: '',
-    received_date: '',
-    ram: '',
-    operating_system: '',
-    screen_size: '',
-    category: '',
+    rentStartDate: "",
+    rentEndDate: "",
+    asset_location: "",
+    processor: "",
+    screen_type: "",
+    received_date: "",
+    ram: "",
+    operating_system: "",
+    screen_size: "",
+    category: "",
     imeiNo: "",
     connectivity: "",
     cableType: "",
@@ -108,32 +102,33 @@ const initialState: InitialState = {
     hdd: "",
     os_version: "",
     make_year: "",
-    is_active:true,
+    is_active: true,
   },
   singleAssetTickets: [],
   employeeassetsdetails: [],
   serviceDetails: [],
-  
+
   serviceticketdetails: {
-    empId: '',
+    empId: "",
     assetId: 0,
     ticketId: 0,
-    title: '',
-    description: '',
-    ticketStatus: '',
-    createdAt: ''
+    title: "",
+    description: "",
+    ticketStatus: "",
+    createdAt: "",
   },
   brandOptions: [],
-  totalSurplusAssetCount:[],
-  totalAssetCount:[],
-  counts:{
-    totalAssets: 0, 
-    ownAssets: 0, 
-    rentedAssets: 0, 
-    surplusAssets: 0, 
+  totalSurplusAssetCount: [],
+  totalAssetCount: [],
+  counts: {
+    totalAssets: 0,
+    ownAssets: 0,
+    rentedAssets: 0,
+    surplusAssets: 0,
     WorkingAssets: 0,
-    RepairabaleAssets: 0, 
-    brokenAssets: 0},
+    RepairabaleAssets: 0,
+    brokenAssets: 0,
+  },
   filterOptions: {
     category: [],
     status: [],
@@ -144,17 +139,13 @@ const initialState: InitialState = {
     asset_location: [],
     operating_system: [],
     cableType: [],
-    connectivity:[],
-    ssd:[],
-    hdd:[],
-    brandName:[]
+    connectivity: [],
+    ssd: [],
+    hdd: [],
+    brandName: [],
   },
-  assetTrasactionLogs:[],
-
-  
-
-
-}
+  assetTrasactionLogs: [],
+};
 
 const adminReducer = (
   state: InitialState = initialState,
@@ -165,76 +156,76 @@ const adminReducer = (
       return {
         ...state,
         loading: true,
-        message: '',
-        error: ''
-      }
+        message: "",
+        error: "",
+      };
 
-      case SET_EMPLOYEES:
+    case SET_EMPLOYEES:
       return {
         ...state,
         employees: action.payload?.data,
-        loading: false
-      }
+        loading: false,
+      };
 
     case SET_ASSETS:
       return {
         ...state,
         assets: action.payload?.data,
-        loading: false
-      }
+        loading: false,
+      };
     case SET_ADDEMPLOYEE:
       return {
         ...state,
         message: action.payload?.message,
-        loading: false
-      }
+        loading: false,
+      };
 
     case UPDATE_ASSET_DETAILS:
       return {
         ...state,
         loading: false,
-        message: action.payload?.message
-      }
+        message: action.payload?.message,
+      };
     case SET_ADDASSET:
       return {
         ...state,
         message: action.payload?.message,
-        loading: false
-      }
+        loading: false,
+      };
 
     case SET_EMPLOYEE_DETAILS:
       return {
         ...state,
         employeeDetails: action.payload?.data,
-        loading: false
-      }
+        loading: false,
+      };
 
     case SET_EMPLOYEE_ASSETS_DETAILS:
       return {
         ...state,
         employeeassetsdetails: action.payload?.data,
-        loading: false
-      }
+        loading: false,
+      };
 
     case SET_SERVICE_TICKET_DETAILS:
       return {
         ...state,
         serviceticketdetails: action.payload?.data,
-        loading: false
-      }
+        loading: false,
+      };
     case SET_SINGLE_ASSET_DETAILS:
       return {
         ...state,
         singleAssetDetails: action.payload?.data,
-        loading: false
-      }
+        loading: false,
+      };
 
-      case SET_SINGLE_ASSET_TICKETS:
+    case SET_SINGLE_ASSET_TICKETS:
       return {
         ...state,
         singleAssetTickets: action.payload?.data,
-        loading: false
-      }
+        loading: false,
+      };
 
     case SET_SERVICE_DETAILS:
       return {
@@ -243,90 +234,87 @@ const adminReducer = (
         loading: false,
       };
 
-      
-        case ASSET_TRANSACTION_HISTORY:
-          return {
-            ...state,
-            assetTrasactionLogs: action.payload?.data,
-            
-            loading: false
-          }
- 
+    case ASSET_TRANSACTION_HISTORY:
+      return {
+        ...state,
+        assetTrasactionLogs: action.payload?.data,
 
+        loading: false,
+      };
 
     case DEALLOCATE_EMPLOYEE_ASSET:
       return {
         ...state,
         message: action.payload?.message,
-        loading: false
-      }
+        loading: false,
+      };
 
     case DELETE_ASSET:
       return {
         ...state,
         message: action.payload?.message,
-        loading: false
-      }
+        loading: false,
+      };
 
-      case DELETE_EMPLOYEE: 
+    case DELETE_EMPLOYEE:
       return {
         ...state,
         message: action.payload?.message,
-        loading: false
-      }
-      
+        loading: false,
+      };
+
     case ALLOCATE_EMPLOYEE_ASSET:
       return {
         ...state,
         message: action.payload?.message,
-        loading: false
-      }
+        loading: false,
+      };
     case SET_TICKET_STATUS:
       return {
         ...state,
         message: action.payload?.message,
-        loading: false
-      }
+        loading: false,
+      };
     case SET_ADD_NOTE:
       return {
         ...state,
         message: action.payload?.message,
-        loading: false
-      }
+        loading: false,
+      };
 
     case SET_ERROR:
       return {
         ...state,
         error: action.payload,
-        loading: false
-      }
+        loading: false,
+      };
 
     case GET_BRAND_OPTIONS:
       return {
         ...state,
         brandOptions: action.payload?.data,
-        loading: false
-      }
+        loading: false,
+      };
 
-      case GET_TOTAL_ASSETSCATEGORY_COUNT:
-        return {
-          ...state,
-          totalAssetCount: action.payload?.data?.totalAssetCount,
-          totalSurplusAssetCount: action.payload?.data?.totalSurplusCount,
-          counts: action.payload?.data?.counts,
-          loading:false
-        }
+    case GET_TOTAL_ASSETSCATEGORY_COUNT:
+      return {
+        ...state,
+        totalAssetCount: action.payload?.data?.totalAssetCount,
+        totalSurplusAssetCount: action.payload?.data?.totalSurplusCount,
+        counts: action.payload?.data?.counts,
+        loading: false,
+      };
 
     case GET_FILTER_OPTIONS:
       return {
         ...state,
         filterOptions: action.payload?.data,
-        loading: false
-      }
+        loading: false,
+      };
 
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default adminReducer
+export default adminReducer;
