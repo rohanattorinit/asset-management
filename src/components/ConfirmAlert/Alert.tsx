a940e073a10a9574b5813bdcdb499ec154b3fd16
Pratiksha Sontakke <pratiksha.sontakke@torinit.ca>
2022-12-16 16:53:35 +0530

    Fixed alert and confirm dialog and transaction history ui

--- src/components/ConfirmAlert/Alert.tsx
+++ src/components/ConfirmAlert/Alert.tsx
@@ -1,54 +1,51 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
-import { TransitionProps } from '@mui/material/transitions';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
     children: React.ReactElement<any, any>;
   },
-  ref: React.Ref<unknown>,
+  ref: React.Ref<unknown>
 ) {
   return <Slide ref={ref} {...props} />;
 });
 
 interface Iprops {
-    title:string,
-    setNavigate: ()=> void
+  title: string;
+  setNavigate: () => void;
 }
 
-export default function Alert(props:Iprops) {
-  
+export default function Alert(props: Iprops) {
   const handleClose = () => {
-    props?.setNavigate()
-
+    props?.setNavigate();
   };
 
   return (
     <>
-     
       <Dialog
         open={true}
         TransitionComponent={Transition}
         keepMounted
         onClose={handleClose}
         aria-describedby="alert-dialog-slide-description"
-        PaperProps={{
-            style: {
-              backgroundColor: '#f3f4f6',
-              boxShadow: '2',
-            },
-          }}
-         sx={{bgcolor: ['Employee deleted Successfully','Asset Deleted Successfully'].includes(props.title) ? 'rgb(0,0,0,0.87)' : 'rgb(0,0,0,0)'}}>
-      
-        <DialogTitle>{props.title}</DialogTitle>
-        
+        sx={{
+          bgcolor: [
+            "Employee deleted successfully",
+            "Asset deleted successfully",
+          ].includes(props.title)
+            ? "rgb(0,0,0,0.87)"
+            : "rgb(0,0,0,0)",
+        }}
+      >
+        <DialogTitle style={{ width: "300px" }}>{props.title}</DialogTitle>
+
         <DialogActions>
-          
-          <Button variant="outlined" onClick={handleClose}>OK</Button>
+          <Button onClick={handleClose}>OK</Button>
         </DialogActions>
       </Dialog>
     </>
--- src/components/ConfirmAlert/Confirm.tsx
+++ src/components/ConfirmAlert/Confirm.tsx
@@ -1,36 +1,33 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
-import { TransitionProps } from '@mui/material/transitions';
-import { makeStyles, Theme } from '@mui/material';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+import { makeStyles, Theme } from "@mui/material";
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
     children: React.ReactElement<any, any>;
   },
-  ref: React.Ref<unknown>,
+  ref: React.Ref<unknown>
 ) {
   return <Slide direction="down" ref={ref} {...props} />;
 });
 interface Iprops {
-    title: string, 
-    handleOk:()=> void
-    handlecancel: () =>void
+  title: string;
+  handleOk: () => void;
+  handlecancel: () => void;
 }
-
-
 
 export default function Confirm(props: Iprops) {
   const [open, setOpen] = React.useState(true);
 
   const handleClose = () => {
-    props.handlecancel()
+    props.handlecancel();
     setOpen(false);
   };
-  
 
   return (
     <div>
@@ -40,10 +37,9 @@ export default function Confirm(props: Iprops) {
         keepMounted
         onClose={handleClose}
         aria-describedby="alert-dialog-slide-description"
-        
       >
-        <DialogTitle>{props.title}</DialogTitle>
-        
+        <DialogTitle style={{ width: "300px" }}>{props.title}</DialogTitle>
+
         <DialogActions>
           <Button onClick={props.handleOk}>OK</Button>
           <Button onClick={handleClose}>CANCEL</Button>
